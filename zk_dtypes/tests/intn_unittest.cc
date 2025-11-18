7ca3786b2fcf7c0cac73b7b7033a4b1eb3da5420
Ryan Kim <chokobole33@gmail.com>
2025-11-18 12:28:20 +0900

    chore: add license & missing license notices to source files
    Adds the Apache License, Version 2.0 license header notice to all source
    files within the zk_dtypes repository.

--- zk_dtypes/tests/intn_unittest.cc
+++ zk_dtypes/tests/intn_unittest.cc
@@ -1,4 +1,5 @@
 /* Copyright 2023 The ml_dtypes Authors. All Rights Reserved.
+Copyright 2025 The zk_dtypes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
